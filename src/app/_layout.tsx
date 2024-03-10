772248f172f2d7ac45977eb22bf561230a532c3f
Edlâneo Manuel <edlanioj@gmail.com>
2024-03-10 21:12:19 +0100

    feat: add styles configurations
    add poppins on  weights regular, medium and bold
    
    add colors and sizes

---
+++ src/app/_layout.tsx
@@ -0,0 +1,19 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { Slot } from 'expo-router';
+
+export default function Layout() {
+  const [isFontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!isFontsLoaded) return null;
+
+  return <Slot />;
+}
